e95c3e4b046c3e12d302bfcc4b500071270e6455
vcnovaes <vcn2@cin.ufpe.br>
2023-04-01 17:13:53 -0300

    Promotions table

---
+++ server/db/sql/promotions.sql
@@ -0,0 +1,9 @@
+CREATE TABLE
+    promotions(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name VARCHAR(255) NOT NULL,
+        category VARCHAR(255) NOT NULL,
+        user_email VARCHAR(255) NOT NULL,
+        is_percent BOOLEAN,
+        value FLOAT
+    );
