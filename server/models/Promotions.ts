35910d4cf18003bafa458c9cba62da389db0b6c4
vcnovaes <vcn2@cin.ufpe.br>
2023-04-01 17:15:32 -0300

    Promotions service performs CRUD operations on DB

--- server/models/Promotions.ts
+++ server/models/Promotions.ts
@@ -1,18 +1,102 @@
-import envs from "../config/env"
-import DatabaseClient from "../db/client";
+import envs from "../config/env.js"
+import DatabaseClient from "../db/client.js";
 import { Promotion } from "../types/promotions";
 
 export default class PromotionsService {
   private dbClient: DatabaseClient;
   constructor() {
-    this.dbClient = new DatabaseClient(envs.DB_URL)
+    this.dbClient = new DatabaseClient(envs.DATABASE_URL)
+    console.info(envs.DATABASE_URL)
   }
-  public insert(promotion: Promotion) {
-    this.dbClient.connect()
+  public async insert(promotion: Promotion) {
+
+    console.info(Object.values(promotion))
+    return new Promise<void>(
+      (resolve, reject) => {
+        this.dbClient.connect()
+          .run(`INSERT INTO promotions(name,user_email, value, is_percent, category) VALUES (?,?,?,?,?)`, Object.values(promotion),
+            (error) => {
+              if (error) {
+                console.error(error)
+                reject(error)
+              }
+              resolve()
+            }
+          ).close()
+        resolve()
+      })
+  }
+  public get(promotionId: number) {
+    return new Promise<Promotion[]>(
+      (resolve, reject) => {
+        this.dbClient.connect()
+          .get(`SELECT * from promotions WHERE id=(?)`, [promotionId],
+            (err, data: Promotion[]) => {
+              if (err) {
+                console.error(err)
+                reject(err)
+              }
+              resolve(data)
+            }
+          ).close()
+      }
+    )
 
   }
-  public get(promotionId: number) { }
-  public getAll() { }
-  public update(promotion: Promotion) { }
-  public delete(promotionName: string) { }
+  public getAll() {
+    return new Promise<Promotion[]>(
+      (resolve, reject) => {
+        this.dbClient.connect()
+          .all(`SELECT * from promotions`,
+            (err, data: Promotion[]) => {
+              if (err) {
+                console.error(err)
+                reject(err)
+              }
+              resolve(data)
+            }
+          ).close()
+      }
+    )
+  }
+  public update(promotion: Promotion) {
+    return new Promise<boolean>(
+      (resolve, reject) => {
+        this.dbClient.connect()
+          .run(`UPDATE promotions SET name=(?) , value=(?), is_percent=(?), category=(?)  WHERE id=(?)`,
+            [
+              promotion.name,
+              promotion.value,
+              promotion.isPercent,
+              promotion.category,
+              promotion.id
+            ],
+            (err) => {
+              if (err) {
+                console.error(err)
+                reject(false)
+              }
+              resolve(true)
+            }
+          ).close()
+      }
+    )
+  }
+
+  public delete(promotionId: number) {
+    return new Promise<boolean>(
+      (resolve, reject) => {
+        this.dbClient.connect()
+          .run(`DELETE from promotions WHERE id=(?)`, [promotionId],
+            (err) => {
+              if (err) {
+                console.error(err)
+                reject(false)
+              }
+              resolve(true)
+            }
+          ).close()
+      }
+    )
+  }
 }
