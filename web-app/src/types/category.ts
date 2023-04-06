85b38807dd2cf0ebeb8fa52e1ca3c398cf4c9727
RenatoNogFer <rnfo@cin.ufpe.br>
2023-04-06 15:48:19 -0300

    Criação de tipos para integração de categorias com a API

---
+++ web-app/src/types/category.ts
@@ -0,0 +1,10 @@
+export interface Category {
+      id: number;
+      name: string;
+      description: string;
+}
+
+export interface AddCategory {
+      name: string;
+      description: string;
+}
