42bcf9e5072f675a9c065c78ac4cb2b8194dba1f
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-04-08 11:52:25 -0300

    Organize item routes

--- web-app/src/router.tsx
+++ web-app/src/router.tsx
@@ -18,8 +18,11 @@ const router = createBrowserRouter(
       <Route path="/" element={<h1>Main Layout</h1>}>
         {/* Main Layout related */}
         <Route path="/home" element={<h1>Home Page</h1>} />
+
         <Route path="/products" element={<h1>Products</h1>} />
         <Route path="/product/:id" element={<SingleProduct />} />
+        <Route path="/product/edit/:id" element={<h1>Edit product Page</h1>} />
+
         <Route path="/search" element={<h1>Search Listing</h1>} />
       </Route>
       <Route path="/login" element={<h1>Common user login</h1>} />
