18673a6fc9998230e1a7bb034e994056595307b5
RenatoNogFer <rnfo@cin.ufpe.br>
2023-04-08 09:53:40 -0300

    Mudança na tabela de categorias para receber id do fornecedor

--- server/db/sql/categories.sql
+++ server/db/sql/categories.sql
@@ -2,4 +2,6 @@ CREATE TABLE IF NOT EXISTS Category (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       name VARCHAR(100) NOT NULL UNIQUE,
       description VARCHAR(255) NOT NULL
+      supplierId INTEGER
+      FOREIGN KEY(supplierId) REFERENCES Supplier(id),
 );
