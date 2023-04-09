af88a3f3a9147ea77d64ab35941ac112096b5899
becca-ls <rls7@cin.ufpe.br>
2023-04-08 22:09:43 -0300

    ajuste no router

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -4,8 +4,20 @@ import "./index.css";
 
 import appRouter from "./router";
 
+
+import { Header } from './pages/components/header/Header';
+import { Links } from './pages/components/links/Links';
+import { MainSupplier } from './pages/main_fornecedores/MainSupplier';
+import { Categories } from './pages/categories/Categories';
+import Promotions from './pages/promotions/Promotions';
+import router from "./router";
 function App() {
-  return <RouterProvider router={appRouter} />;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+
 }
 
 export default App;
