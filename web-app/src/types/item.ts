1930f3218df314b8276b236f0ef553ff5a2fea80
Murilo Barbosa <mbn2@cin.ufpe.br>
2023-04-09 16:34:25 -0300

    Add front-end item service

---
+++ web-app/src/types/item.ts
@@ -0,0 +1,23 @@
+import { Category } from './category';
+
+export interface Item {
+      id: number;
+      picture?: string;
+      name: string;
+      desc: string;
+      price: number;
+      inStock: number;
+      supplierId: number;
+      categories: Category[];
+      promotion?: any;
+}
+
+export interface AddItem {
+      picture?: string;
+      name: string;
+      desc: string;
+      price: number;
+      inStock: number;
+      supplierId: number;
+      categories: number[]; // array de ids
+};
