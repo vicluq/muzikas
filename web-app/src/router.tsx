1a9092bea3940e5a77ab003266893abf6ff6f98a
vicluq <victoria.tewari@gmail.com>
2023-04-09 14:19:17 -0300

    router

--- web-app/src/router.tsx
+++ web-app/src/router.tsx
@@ -2,15 +2,15 @@ import {
   Route,
   createRoutesFromElements,
   createBrowserRouter,
-} from "react-router-dom";
+} from 'react-router-dom'
 
-import ProtectedRoute from "./components/ProtectedRoute";
-import SupplierLayout from "./components/SupplierLayout";
-import LoginSuppliers from "./pages/LoginSuppliers/LoginSuppliers";
-import SingleProduct from './pages/products/SingleProduct';
-import { RegisterSupplier } from "./pages/RegisterSupplier/RegisterSupplier";
-import { Categories } from './pages/categories/Categories';
-
+import ProtectedRoute from './components/ProtectedRoute'
+import SupplierLayout from './components/SupplierLayout'
+import LoginSuppliers from './pages/LoginSuppliers/LoginSuppliers'
+import SingleProduct from './pages/products/SingleProduct'
+import { RegisterSupplier } from './pages/RegisterSupplier/RegisterSupplier'
+import { Categories } from './pages/categories/Categories'
+import Search from './pages/Search'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -18,13 +18,11 @@ const router = createBrowserRouter(
       <Route path="/" element={<h1>Main Layout</h1>}>
         {/* Main Layout related */}
         <Route path="/home" element={<h1>Home Page</h1>} />
-
         <Route path="/products" element={<h1>Products</h1>} />
         <Route path="/product/:id" element={<SingleProduct />} />
         <Route path="/product/edit/:id" element={<h1>Edit product Page</h1>} />
-
-        <Route path="/search" element={<h1>Search Listing</h1>} />
       </Route>
+      <Route path="/search" element={<Search />} />
       <Route path="/login" element={<h1>Common user login</h1>} />
       <Route path="/register" element={<h1>Supplier login</h1>} />
       <Route path="/supplier/login" element={<LoginSuppliers />} />
@@ -39,8 +37,8 @@ const router = createBrowserRouter(
       >
         <Route path="/supplier/categories" element={<Categories />} />
       </Route>
-    </Route>
-  )
-);
+    </Route>,
+  ),
+)
 
-export default router;
+export default router
