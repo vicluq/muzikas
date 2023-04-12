4faa435f38a05d715ab7c5da5e63ababf0bc0cbd
vicluq <victoria.tewari@gmail.com>
2023-04-11 23:11:25 -0300

    Search page created

--- web-app/src/pages/Search/index.tsx
+++ web-app/src/pages/Search/index.tsx
@@ -1,21 +1,42 @@
 import { useState, useEffect } from 'react'
 import { createSearchParams, useSearchParams } from 'react-router-dom'
+import ItemService from './../../services/api/item.service'
+import { Item } from './../../types/item'
 
 const Search = () => {
   // Instanciat Item Sercive (ver exemplo das categorias)
+  const [items, setItems] = useState<Item[]>([])
   const [search, setSearch] = useState('')
   const [searchParams, setSearchParams] = useSearchParams()
+  const [feedback, setFeedback] = useState('')
+  const [error, setError] = useState(false)
 
   const searchHandler = () => {
     setSearchParams(createSearchParams({ query: search }))
   }
 
+  const itemService = new ItemService()
+
+  const getItems = async () => {
+    let response: any = null
+
+    try {
+      response = await itemService.getAll()
+      if (response.errorType) {
+        setFeedback(response.message)
+        setError(true)
+      } else {
+        setItems(response)
+      }
+    } catch (e) {
+      setFeedback(response.message)
+      setError(true)
+    }
+  }
+
   useEffect(() => {
-    const query = searchParams.get('query')
-    // Pegar produtos da API
-    // Passar query para a funcao
-    // se tiver query, no item service adicionar `?query=${query}` no fim da URL do getItems
-  }, [searchParams])
+    getItems();
+  }, [])
 
   return (
     <div>
@@ -29,10 +50,18 @@ const Search = () => {
           <button onClick={searchHandler}>Buscar</button>
         </div>
         <div>
-            <h2>Search results for "{searchParams.get('query')}"</h2>
+          <h2>Search results for "{searchParams.get('query')}"</h2>
         </div>
       </div>
-      <div className="product-list"></div>
+      <div className="product-list">
+        {items
+          .filter((item: Item) =>
+            item.name.includes(searchParams.get('query') || ''),
+          )
+          .map((item) => (
+            <div>{item.name}</div>
+          ))}
+      </div>
     </div>
   )
 }
